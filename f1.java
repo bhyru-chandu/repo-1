8466b8b0f7fd530f6ac6c25ef720467176785cba
chandu <bhyruchandu@gmail.com>
2025-04-21 15:44:35 +0530

    this is commiting

--- f1.java
+++ f1.java
@@ -1,5 +1,21 @@
+
 class A
 {
     int i;
     int j;
+}
+
+class F1{
+    public void main()
+    {
+        System.out.println("this is chandu here");
+    }
+
+}
+
+class F2{
+    public void m2()
+    {
+        System.out.println("this is another chandu");
+    }
 }
